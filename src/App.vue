e401000633ce2409e37aeb4e5710258914a5df01
gspenguin <97851722+gspenguin@users.noreply.github.com>
2022-10-03 20:11:32 -0400

    colors

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,17 @@ export default {
 </script>
 
 <style>
+  body  {
+    background-color: orange;
+  }
 #app {
+  background-color: tan;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 75%;
 }
 </style>
