3b4ce527900c31fd09b202482393dbb5000ff1ea
gspenguin <97851722+gspenguin@users.noreply.github.com>
2022-10-07 22:52:46 -0400

    Adding a form

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
+
   <h1>Costume Idea Generator</h1>
   Not sure what your halloween Costume should be?
+
+  <Form />
 </template>
 
 <script>
+import Form from './components/Form'
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    Form
+  },
+};
 </script>
 
 <style>
   body  {
     background-color: orange;
+    margin: 0;
+    padding: 0;
+    min-height: 90vh
   }
-#app {
-  background-color: tan;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  padding-top: 60px;
-  width: 75%;
-  margin: 0 auto;
-}
+  #app {
+    background-color: tan;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    padding-top: 60px;
+    width: 75%;
+    margin: 0 auto;
+  }
 </style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Form',
   props: {
     msg: String
   }
